72ec6819b47028d5b965c599fcb893bb16395060
Tarun Bhalla <tarunbhalla96@gmail.com>
2021-06-17 12:59:37 -0400

    update

--- resources/script/script.js
+++ resources/script/script.js
@@ -10,3 +10,7 @@ $(function () {
 });
 
 
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
